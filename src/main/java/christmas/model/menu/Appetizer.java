517cf08f6414eeaa449a7f5ec2eeff8d6f2159d7
HongYeseul <ghd8119@naver.com>
2023-11-12 16:35:52 +0900

    :sparkles: feat(menu): 주문 가능한 메뉴 등록

--- src/main/java/christmas/model/menu/Appetizer.java
+++ src/main/java/christmas/model/menu/Appetizer.java
@@ -1,4 +1,23 @@
 package christmas.model.menu;
 
 public enum Appetizer {
+    BUTTON_MUSHROOM_SOUP("양송이수프", 6_000),
+    TAPAS("타파스", 5_500),
+    CAESAR_SALAD("시저샐러드", 8_000);
+
+    private final String appetizerName;
+    private final Integer appetizerPrice;
+
+    Appetizer(String appetizerName, Integer appetizerPrice){
+        this.appetizerName = appetizerName;
+        this.appetizerPrice = appetizerPrice;
+    }
+
+    public String getName(){
+        return appetizerName;
+    }
+
+    public Integer getPrice(){
+        return appetizerPrice;
+    }
 }
