b138a0c62c09f2e80bd5034e66c1903a1c6e5eb9
HongYeseul <ghd8119@naver.com>
2023-11-12 22:32:18 +0900

    :sparkles: feat(OrderDateService): 크리스마스 디데이 할인 금액 계산하는 기능 추가

--- src/main/java/christmas/model/customer/OrderDate.java
+++ src/main/java/christmas/model/customer/OrderDate.java
@@ -1,8 +1,14 @@
 package christmas.model.customer;
 
+import java.math.BigDecimal;
+
 public class OrderDate {
     private final Integer date;
     public OrderDate(Integer date) {
         this.date = date;
+    }
+
+    public BigDecimal saleFromXMasDDay() {
+        return new BigDecimal(Integer.toString((date - 1) * 100));
     }
 }
--- src/main/java/christmas/service/OrderDateService.java
+++ src/main/java/christmas/service/OrderDateService.java
@@ -4,13 +4,20 @@ import christmas.DTO.OrderDateRequestDTO;
 import christmas.DTO.OrderDateResponseDTO;
 import christmas.model.customer.OrderDate;
 
+import java.math.BigDecimal;
+
 public class OrderDateService {
-    private static OrderDate orderDate;
+    private OrderDate orderDate;
     /*
     * inputOrderDate: 고객이 식당 방문 날짜 입력시 DateDTO -> OrderDate 저장
     * */
-    public static OrderDateResponseDTO inputOrderDate(OrderDateRequestDTO requestDTO){
+    public OrderDateResponseDTO inputOrderDate(OrderDateRequestDTO requestDTO){
         orderDate = new OrderDate(requestDTO.date());
         return new OrderDateResponseDTO(requestDTO.date());
+    }
+
+    public BigDecimal calculateDDaySalePrice(OrderDate orderDate) {
+        BigDecimal result = new BigDecimal("1000");
+        return result.add(orderDate.saleFromXMasDDay());
     }
 }
