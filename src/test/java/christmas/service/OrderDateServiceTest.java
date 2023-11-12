e305c7769e0e2bc43038955f57a5f1891ecc927e
HongYeseul <ghd8119@naver.com>
2023-11-12 22:32:01 +0900

    :white_check_mark: test(OrderDateService): 크리스마스 디데이 할인 금액 계산하는 테스트 추가

--- src/test/java/christmas/service/OrderDateServiceTest.java
+++ src/test/java/christmas/service/OrderDateServiceTest.java
@@ -2,17 +2,54 @@ package christmas.service;
 
 import christmas.DTO.OrderDateRequestDTO;
 import christmas.DTO.OrderDateResponseDTO;
+import christmas.model.customer.OrderDate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OrderDateServiceTest {
+    OrderDateService orderDateService = new OrderDateService();
+    OrderDate orderDate;
+
+    @BeforeEach
+    void init(){
+        orderDate = new OrderDate(3);
+    }
+
     @Test
-    @DisplayName("[SUCESS] 날짜를 정상적인 값으로 입력하면 예외가 발생하지 않는다.")
+    @DisplayName("[SUCCESS] 날짜를 정상적인 값으로 입력하면 예외가 발생하지 않는다.")
     void inputNormalDate(){
         OrderDateRequestDTO orderDateRequestDTO = new OrderDateRequestDTO(12);
-        OrderDateResponseDTO orderDateResponseDTO = OrderDateService.inputOrderDate(orderDateRequestDTO);
+        OrderDateResponseDTO orderDateResponseDTO = orderDateService.inputOrderDate(orderDateRequestDTO);
         assertThat(orderDateResponseDTO.date()).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("[SUCCESS] 크리스마스 디데이 할인 금액을 계산한다.")
+    void calculateDDaySalePrice(){
+        BigDecimal result = orderDateService.calculateDDaySalePrice(orderDate);
+        assertThat(result).isEqualTo(new BigDecimal("1200"));
+    }
+
+    @Test
+    @DisplayName("[SUCCESS] 평일 할인 금액을 계산한다.")
+    void calculateWeekDaySalePrice(){
+
+    }
+
+    @Test
+    @DisplayName("[SUCCESS] 주말 할인 금액을 계산한다.")
+    void calculateWeekendSalePrice(){
+
+    }
+
+    @Test
+    @DisplayName("[SUCCESS] 특별 할인 금액을 계산한다.")
+    void calculateSpecialSalePrice(){
+
     }
 }
