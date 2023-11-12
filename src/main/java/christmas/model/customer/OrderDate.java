b068cba51db9f207421f3097139fd71c65182de5
HongYeseul <ghd8119@naver.com>
2023-11-12 23:27:13 +0900

    :white_check_mark: feat(OrderDateService): 평일 할인 금액 계산하는 기능 추가 - OrderDate: 평일인지 판단하는 메서드 생성 - Week: 요일 계산하는 로직 enum 생성

--- src/main/java/christmas/model/customer/OrderDate.java
+++ src/main/java/christmas/model/customer/OrderDate.java
@@ -1,6 +1,9 @@
 package christmas.model.customer;
 
+import christmas.model.Week;
+
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class OrderDate {
     private final Integer date;
@@ -10,5 +13,15 @@ public class OrderDate {
 
     public BigDecimal saleFromXMasDDay() {
         return new BigDecimal(Integer.toString((date - 1) * 100));
+    }
+
+    public boolean isWeekDay() {
+        LocalDate localDate = LocalDate.of(2023, 12, date);
+        Week dayOfWeek = Week.getDay(localDate.getDayOfWeek().getValue());
+        return dayOfWeek == Week.MONDAY
+                || dayOfWeek == Week.TUESDAY
+                || dayOfWeek == Week.WEDNESDAY
+                || dayOfWeek == Week.THURSDAY
+                || dayOfWeek == Week.SUNDAY;
     }
 }
--- src/main/java/christmas/service/OrderDateService.java
+++ src/main/java/christmas/service/OrderDateService.java
@@ -5,6 +5,7 @@ import christmas.DTO.OrderDateResponseDTO;
 import christmas.model.customer.OrderDate;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class OrderDateService {
     private OrderDate orderDate;
@@ -16,8 +17,22 @@ public class OrderDateService {
         return new OrderDateResponseDTO(requestDTO.date());
     }
 
+    /**
+     * 크리스마스 디데이 할인 금액 계산
+     * @param orderDate
+     * @return
+     */
     public BigDecimal calculateDDaySalePrice(OrderDate orderDate) {
+        // TODO: 크리스마스 이후 할인 없음
         BigDecimal result = new BigDecimal("1000");
         return result.add(orderDate.saleFromXMasDDay());
+    }
+
+    public BigDecimal calculateWeekDaySalePrice(OrderDate orderDate, Integer dessertCount) {
+        BigDecimal result = new BigDecimal("2023");
+        if (orderDate.isWeekDay()) {
+            return result.multiply(BigDecimal.valueOf(dessertCount));
+        }
+        return new BigDecimal("0");
     }
 }
