fbed5f9652db1814ee3bca0ab46a17e57987932f
HongYeseul <ghd8119@naver.com>
2023-11-13 00:11:40 +0900

    :sparkles: feat(OrderDateService): 총 혜택 금액 계산하는 기능 추가

--- src/main/java/christmas/service/OrderDateService.java
+++ src/main/java/christmas/service/OrderDateService.java
@@ -3,6 +3,7 @@ package christmas.service;
 import christmas.DTO.OrderDateRequestDTO;
 import christmas.DTO.OrderDateResponseDTO;
 import christmas.model.customer.OrderDate;
+import christmas.model.menu.Menu;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -49,5 +50,18 @@ public class OrderDateService {
             return new BigDecimal("1000");
         }
         return new BigDecimal("0");
+    }
+
+    public BigDecimal calculateTotalSalePrice(
+            BigDecimal dDaySale,
+            BigDecimal weekDaySale,
+            BigDecimal weekendSale,
+            BigDecimal specialSale,
+            boolean hasAdditionalGift) {
+        BigDecimal result = dDaySale.add(weekDaySale).add(weekendSale).add(specialSale);
+        if (hasAdditionalGift) {
+            return result.add(new BigDecimal(Menu.CHAMPAGNE.getPrice().toString()));
+        }
+        return result;
     }
 }
