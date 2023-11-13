a181401f731430768696d70e89220a7ef1736325
HongYeseul <ghd8119@naver.com>
2023-11-13 21:35:30 +0900

    :art: style(MenuValidator): 공백 제거

--- src/main/java/christmas/model/validator/MenuValidator.java
+++ src/main/java/christmas/model/validator/MenuValidator.java
@@ -8,7 +8,6 @@ import java.util.Objects;
 import static christmas.model.validator.ExceptionMessage.INVALID_MENU_ERROR_MESSAGE;
 
 public class MenuValidator {
-
     public static void hasMenu(String menuName) {
         if (Arrays.stream(Menu.values())
                 .noneMatch(menu -> Objects.equals(menu.getName(), menuName))) {
