cc5b52c67dcb8d5034358d367d3e915cfb118d6e
HongYeseul <ghd8119@naver.com>
2023-11-13 21:34:37 +0900

    :recycle: refactor(DateValidator): 매직 넘버 상수 교체

--- src/main/java/christmas/model/constants/Day.java
+++ src/main/java/christmas/model/constants/Day.java
@@ -6,4 +6,7 @@ public class Day {
     public static final Integer YEAR = 2023;
     public static final Integer MONTH = 12;
     public static final Integer CHRISTMAS_DAY = 25;
+
+    public static final Integer FIRST_DAY_OF_DECEMBER = 1;
+    public static final Integer LAST_DAY_OF_DECEMBER = 31;
 }
--- src/main/java/christmas/model/validator/DateValidator.java
+++ src/main/java/christmas/model/validator/DateValidator.java
@@ -1,10 +1,12 @@
 package christmas.model.validator;
 
+import static christmas.model.constants.Day.FIRST_DAY_OF_DECEMBER;
+import static christmas.model.constants.Day.LAST_DAY_OF_DECEMBER;
 import static christmas.view.validator.ExceptionMessage.INVALID_DATE_ERROR_MESSAGE;
 
 public class DateValidator {
     public static void checkValidDate(Integer date){
-        if( date > 31 || date < 1){
+        if(date < FIRST_DAY_OF_DECEMBER || date > LAST_DAY_OF_DECEMBER){
             throw new IllegalArgumentException(INVALID_DATE_ERROR_MESSAGE);
         }
     }
