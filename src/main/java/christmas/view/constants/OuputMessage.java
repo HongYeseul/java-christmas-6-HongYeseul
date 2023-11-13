69c7c80c7ee00dcc984282b3651d1fef37275236
HongYeseul <ghd8119@naver.com>
2023-11-13 09:57:14 +0900

    :art: style(OutputMessage): 출력 포맷 변경

--- src/main/java/christmas/view/constants/OuputMessage.java
+++ src/main/java/christmas/view/constants/OuputMessage.java
@@ -4,8 +4,7 @@ public class OuputMessage {
     // TODO: 12월은 유지보수 가능하도록 상수 치환
     public static final String START_EVENT_PLANNER_MESSAGE = """
             안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.
-            12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)
-            """;
+            12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)""";
 
     public static final String ASK_MENU_AND_COUNT = """
             주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)
