2b05e29713b83decc71bc6bd61e58cd6c7d49686
HongYeseul <ghd8119@naver.com>
2023-11-14 00:03:01 +0900

    :bug: fix(Regex): 고객 주문시 0개 주문 불가능하도록 수정

--- src/main/java/christmas/view/constants/Regex.java
+++ src/main/java/christmas/view/constants/Regex.java
@@ -2,5 +2,5 @@ package christmas.view.constants;
 
 public class Regex {
     public static final String REGEX_DATE_NUMERIC = "([1-3]\\d)|(^[1-9])";
-    public static final String REGEX_ORDER_FORMAT = "(([ㄱ-ㅎ가-힣]){1,}-(\\d){1,}(,)?){1,}";
+    public static final String REGEX_ORDER_FORMAT = "(([ㄱ-ㅎ가-힣]){1,}-([1-9]\\d?)(,)?){1,}";
 }
