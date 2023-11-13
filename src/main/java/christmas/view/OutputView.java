36d06af9cbcfc72b5813aa9dc6e3275935440483
HongYeseul <ghd8119@naver.com>
2023-11-13 09:57:39 +0900

    :art: style(OuputView): static -> 일반 메서드로 변경

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,11 +3,11 @@ package christmas.view;
 import static christmas.view.constants.OuputMessage.START_EVENT_PLANNER_MESSAGE;
 
 public class OutputView {
-    public static void startEventPlanner(){
+    public void startEventPlanner(){
         print(START_EVENT_PLANNER_MESSAGE);
     }
 
-    private static void print(String message) {
+    private void print(String message) {
         System.out.println(message);
     }
 }
