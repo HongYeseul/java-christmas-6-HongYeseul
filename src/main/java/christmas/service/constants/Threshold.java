4a9cfe5be4463f64dbbde187d6a15ad1125925af
HongYeseul <ghd8119@naver.com>
2023-11-13 22:21:19 +0900

    :art: style(service/Threshold): 오타 수정

--- src/main/java/christmas/service/constants/Threshold.java
+++ src/main/java/christmas/service/constants/Threshold.java
@@ -6,7 +6,7 @@ public class Threshold {
     public static final BigDecimal SPECIAL_GIFT_THRESHOLD = new BigDecimal("120000");
 
     /**
-     * 기준값보다 이상인지 판단하는 용으로 사용되는 상
+     * 기준값보다 이상인지 판단하는 용으로 사용되는 상수
      */
     public static final Integer BIG_DECIMAL_RESULT_THRESHOLD = 0;
 }
