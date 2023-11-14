66211413ba5a67a90cf7b29b70609728be4d915d
HongYeseul <ghd8119@naver.com>
2023-11-14 16:02:21 +0900

    :art: style(ApplicationTest): 필요 없는 코드 삭제

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -2,10 +2,8 @@ package christmas;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import christmas.dto.OrderMenuInputDTO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
